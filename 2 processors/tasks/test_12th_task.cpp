8036b004b1257342a5f0e7ac4ea7012c0f652cca
burmyakov <artem.burmyakov@gmail.com>
2022-04-18 22:30:32 +0300

    types made consistent

--- 2 processors/tasks/test_12th_task.cpp
+++ 2 processors/tasks/test_12th_task.cpp
@@ -189,7 +189,7 @@ bool find_dominating_state(const state& s, const unsigned short m, const TS& ts,
     unsigned int keysNum_pj = 0;
     get_keys_pj(s, m, keysNum_pj, bKeys_pj);
     
-    for (int i25 = 0; i25 < keysNum_pj; i25++) {
+    for (unsigned int i25 = 0; i25 < keysNum_pj; i25++) {
         if ((*visitedStates).find((*bKeys_pj)[i25]) != (*visitedStates).end()) {
             if (find_dominating_state_hpt1(s, ts, (*visitedStates)[(*bKeys_pj)[i25]])) return true;
         }
